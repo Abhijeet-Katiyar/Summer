f867e2205d00222ef04db83de772f77d476ebba2
Abhijeet-katiyar <katiyar.abhijeet002@gmail.com>
2018-08-19 00:34:45 +0530

    Commit using Atom

--- Untitled4.py
+++ Untitled4.py
@@ -19,7 +19,7 @@ Nifty_data=pd.read_csv("E:/summer/NIFTY50.csv",parse_dates=['Date'],index_col=['
 # In[3]:
 
 
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf 
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 
 # In[4]:
@@ -55,12 +55,12 @@ def plotds(xt, nlag=30, fig_size=(12, 10)):
          xt = pd.Series(xt)
     plt.figure(figsize=fig_size)
     layout = (2, 2)
-    
+
     # Assign axes
     ax_xt = plt.subplot2grid(layout, (0, 0), colspan=2)
     ax_acf= plt.subplot2grid(layout, (1, 0))
     ax_pacf = plt.subplot2grid(layout, (1, 1))
-    
+
     # Plot graphs
     xt.plot(ax=ax_xt)
     ax_xt.set_title('Time Series')
@@ -122,4 +122,3 @@ import sklearn
 
 
 sklearn.metrics.r2_score(arima_obj1_fit,pred)
-
