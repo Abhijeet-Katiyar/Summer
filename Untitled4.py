e0f7f3bf93566bd7dbfdfe8d92d75b96476e429f
Abhijeet-katiyar <katiyar.abhijeet002@gmail.com>
2018-08-19 00:38:24 +0530

    Commit using Atom

--- Untitled4.py
+++ Untitled4.py
@@ -1,54 +1,21 @@
 
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
-
-# In[2]:
-
-
 Nifty_data=pd.read_csv("E:/summer/NIFTY50.csv",parse_dates=['Date'],index_col=['Date'])
 
-
-# In[3]:
-
-
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-
-
-# In[4]:
-
 
 train=Nifty_data['Close'].iloc[:1750]
 test=Nifty_data['Close'].iloc[1751:]
 
-
-# In[5]:
-
-
 train.tail()
-
-
-# In[6]:
-
 
 test.head()
 
-
-# In[7]:
-
-
 from statsmodels.tsa.arima_model import ARIMA
-
-
-# In[8]:
-
 
 def plotds(xt, nlag=30, fig_size=(12, 10)):
     if not isinstance(xt, pd.Series):
@@ -69,23 +36,11 @@ def plotds(xt, nlag=30, fig_size=(12, 10)):
     plt.tight_layout()
     return None
 
-
-# In[9]:
-
-
 # plotting data
 plotds(Nifty_data['Close'], nlag=50)
 
-
-# In[10]:
-
-
 #plotting QQ plot and probability plot
 sm.qqplot(Nifty_data['Close'], line='s')
-
-
-# In[25]:
-
 
 # Optimize ARIMA parameters
 aicVal=[]
@@ -99,26 +54,10 @@ for d in range(0,3):
             except:
                 pass
 
-
-# In[26]:
-
-
-aicVal
-
-
-# In[27]:
-
+print(aicVal)
 
 pred=np.append([0,0],arima_obj1_fit.fittedvalues.tolist())
 
-
-# In[28]:
-
-
 import sklearn
-
-
-# In[29]:
-
 
 sklearn.metrics.r2_score(arima_obj1_fit,pred)
